a3098c6c6688269ed76716cc1facfc538c571727
Gokul <pgokul@gmail.com>
2012-05-15 18:16:53 +0530

    Added support for author etc

--- ExcelParser.py
+++ ExcelParser.py
@@ -5,6 +5,16 @@ import clr
 clr.AddReference("Microsoft.Office.Interop.Excel")
 import Microsoft.Office.Interop.Excel as Excel
 
+INPUT_FILE = 'if'
+OUTPUT_FOLDER = 'op'
+SOFTCOPY = 'fn'
+DLI_BARCODE = 'dli'
+OTHER_LINKS = 'ol'
+PRABHANDAM = 'pn'
+AUTHOR = 'an'
+PUBLISHER = 'pbn'
+MARAN_LINK = 'mdl'
+
 class excelHandler:
     def __init__(self):
         self.__excel__ = Excel.ApplicationClass()
@@ -34,54 +44,80 @@ def parseOptions():
     args = parser.parse_args();
     return args
 
+def makeLower(args):
+    lowerArgs = {}
+    for k,v in args.iteritems():
+        lowerArgs[k] = v.lower()
+    return lowerArgs
+              
+
+def getHeaderCols(usedRange,args):
+    cols = {}
+    numRows = usedRange.Rows.count
+    numCols = usedRange.Columns.count
+    
+    for ir in range(1,2):
+        for ic in range(1,numCols+1):
+            cellVal = usedRange.Cells(ir,ic).Value()
+            if(cellVal == None):
+                continue
+
+            lowerCellVal = cellVal.lower()
+            for k,v in args.iteritems():
+                if(v == lowerCellVal):
+                    cols[k] = ic
+
+    return cols
+
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc:
+        if exc.errno == errno.EEXIST:
+            pass
+        else: raise
+
 def handleSheet(excel,sheet,baseDir,args):
     name = sheet.name;
     sheetDir = os.path.join(baseDir,name)
     excel.makeSheetActive(sheet)
     usedRange = sheet.UsedRange
     numRows = usedRange.Rows.count
-    numCols = usedRange.Columns.count
-    
-    headerRow = None
-    softCopyCol = None
+    curPrabhandam = None
 
-    dliCol = None
-    mdlCol = None
-    linkCells = []
+    headerCols = getHeaderCols(usedRange,args)
     # try to get the coloum number that has the value "SoftCopy"
-    for ir in range(1,numRows+1):
-        for ic in range(1,numCols+1):
-            cellVal = usedRange.Cells(ir,ic).Value()
-            print(cellVal)
-            if(cellVal == None):
-                continue
+    for ir in range(2,numRows+1):
+        cellVal = usedRange.Cells(ir,headerCols[PRABHANDAM]).Value()
+        if(cellVal != None and cellVal != ""):
+            curPrabhandam = cellVal
+        if(usedRange.Cells(ir,headerCols[SOFTCOPY]).Value() == "Y"):
+            author = usedRange.Cells(ir,headerCols[AUTHOR]).Value()
+            publisher = usedRange.Cells(ir,headerCols[PUBLISHER]).Value()
+            filepath = os.path.join(sheetDir,curPrabhandam,author,publisher)
+            mkdir_p(filepath)
+            print("Have a soft copy for the prabhandam:"+curPrabhandam)
+            #We have a soft copy
 
-            lowerCellVal = cellVal.lower()
-            if(args['fn'] == lowerCellVal):
-                headerRow = ir
-                softCopyCol = ic
-            elif(lowerCellVal == args['dli']):
-                dliCol = ic
-            elif(lowerCellVal == args['mdl']):
-                mdlCol = ic
-            else:
-                print(lowerCellVal)
-
-    os.makedirs(sheetDir)
+    #os.makedirs(sheetDir)
 
 def main():
     args = {}
     if(len(sys.argv) != 1):
         args = parseOptions();
     else:
-        args['if']= 'c:\DhivyaPrabhandam.xlsx'
-        args['op'] = 'c:\output'
-        args['fn'] = 'softcopy'
-        args['dli'] = 'DLI Barcode'
-        args['mdl'] =  """Maran's Dog link"""
-        args['ol'] = 'Other Links'
+        args[INPUT_FILE]= 'F:\DhivyaPrabhandam.xlsx'
+        args[OUTPUT_FOLDER] = 'F:\output'
+        args[SOFTCOPY] = 'softcopy'
+        args[DLI_BARCODE] = 'DLI Barcode'
+        args[MARAN_LINK] =  """Maran's Dog link"""
+        args[OTHER_LINKS] = 'Other Links'
+        args[PRABHANDAM] = 'Prabhandam'
+        args[AUTHOR] = 'Author'
+        args[PUBLISHER] = 'Publisher'
 
-    basePath = args['op']
+    args = makeLower(args)
+    basePath = args[OUTPUT_FOLDER]
     excel = excelHandler()
 
     baseDir = os.path.join(basePath,excel.open(args['if']))
